13fabd316067e32ce1775b6571be58a0a16cdfcb
Pratham Srivastava <117106015+pratham-srivastava-07@users.noreply.github.com>
2023-12-29 14:07:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser') 
+const https = require('https')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true})) // took from youtube
+
+app.get('/', (req, res)=> {
+   res.sendFile(__dirname + '/index.html')
+})
+app.post('/', (req, res)=> {
+    const city =  req.body.cityName
+const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=f58f365721b043ba31e472b34371b343'
+https.get(url, (response)=> {
+    response.on('data', (data)=> {
+        const whetherData =JSON.parse(data)
+        console.log(whetherData);
+        let temp = whetherData.main.temp
+        res.send(`<h1>Temperature in ${city} is ${temp}</h1>`)
+    })
+})
+})
+
+app.listen(5000, ()=> {
+    console.log('server is listening on port 5000');
+})
+
+
+
+// 'https://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=f58f365721b043ba31e472b34371b343'
